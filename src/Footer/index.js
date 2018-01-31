089ce0129494178cefc8a942b37068dd1e7427aa
lucasjs <lucas.tcjb@gmail.com>
2018-01-31 17:21:02 +0000

    footer: add component documentation

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -4,6 +4,10 @@ import ThemeConsumer from '../ThemeConsumer'
 
 const consumeTheme = ThemeConsumer('UIFooter')
 
+/**
+ * The footer is shown at the bottom of the page. It displays
+ * links to other pages.
+*/
 const Footer = ({ theme, children, links }) => (
   <footer className={theme.footer}>
     <nav>
@@ -26,13 +30,28 @@ const Footer = ({ theme, children, links }) => (
 )
 
 Footer.propTypes = {
+  /**
+   * @see [ThemeProvider](#themeprovider) - Theme received from consumeTheme wrapper.
+   */
   theme: PropTypes.shape({
     footer: PropTypes.string,
   }),
+  /**
+   * The footer navigation links. It should contain an array with the links titles and events.
+   */
   links: PropTypes.arrayOf(PropTypes.shape({
+    /**
+     * The visible navigation link title.
+    */
     title: PropTypes.string,
+    /**
+     * The navigation link event.
+    */
     onClick: PropTypes.func,
   })).isRequired,
+  /**
+   * The component children. It should contain a React element.
+  */
   children: PropTypes.node.isRequired,
 }
 
