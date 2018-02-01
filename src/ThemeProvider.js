4519849a5ad5ccc5b5cfe0e5dc4fe08bd4bd5e71
Deivis <deiviswingert@hotmail.com>
2018-02-01 16:15:04 -0200

    theme-provider: add component documentation

--- src/ThemeProvider.js
+++ src/ThemeProvider.js
@@ -20,37 +20,32 @@ export default class ThemeProvider extends React.Component {
   }
 }
 
-/**
- * This object is a compiled of styles which came from the provided theme.
- * All of the theme props are strings which mirror the theme css classes.
- * In wrapped components props, this object can be accessed by the `theme` prop.
- * @typedef {object} Styles
- */
-
-/**
- * This object is a set of icons provided in the theme object.
- * The icons are all react elements which have an icon inside.
- * In wrapped components props, this object can be accessed by the `icons` prop.
- * @typedef {object} Icons
- */
-
-/**
- * A magical object received by the provider and passed to all of its children.
- * It's responsible for all css and icons default customizations, uses
- * the react context API to propagate it own props to the children.
- *
- * @typedef {object} Theme
- * @prop {Style} styles
- * @prop {Icons} icons
- * @prop {string} name
- * @prop {string} version
- */
-
 ThemeProvider.childContextTypes = {
+  /**
+   * A magical object received by the provider and passed to all of its children.
+   * It's responsible for all css and icons default customizations, uses
+   * the react context API to propagate it own props to the children.
+   */
   theme: shape({
+    /**
+     * Theme name/description
+     */
     name: string,
+    /**
+     * Theme version
+     */
     version: string,
+    /**
+     * This object is a compiled of styles which came from the provided theme.
+     * All of the theme props are strings which mirror the theme css classes.
+     * In wrapped components props, this object can be accessed by the `theme` prop.
+     */
     styles: object, // eslint-disable-line
+    /**
+     * This object is a set of icons provided in the theme object.
+     * The icons are all react elements which have an icon inside.
+     * In wrapped components props, this object can be accessed by the `icons` prop.
+     */
     icons: object, // eslint-disable-line
   }),
 }
