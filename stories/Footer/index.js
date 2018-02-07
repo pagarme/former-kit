a69ac743660d66ed98670098f36faf5d60773526
Leonardo Felipe Nerone <leonardofelipenerone@gmail.com>
2018-02-07 21:27:57 -0200

    footer: add Section to story

--- stories/Footer/index.js
+++ stories/Footer/index.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 
+import Section from '../Section'
 import FooterExample from './FooterExample'
 
 storiesOf('Footer', module)
-  .add('defaultTheme', () => <FooterExample />)
+  .add('Default', () => (
+    <Section title="Only example">
+      <FooterExample />
+    </Section>
+  ))
