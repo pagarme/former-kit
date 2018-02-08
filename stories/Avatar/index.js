0d0b74341bd5996946b1dfde82f2685c49e0430c
Leonardo Felipe Nerone <leonardofelipenerone@gmail.com>
2018-02-08 11:09:45 -0200

    avatar: add Section to story

--- stories/Avatar/index.js
+++ stories/Avatar/index.js
@@ -1,18 +1,26 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 
+import Section from '../Section'
 import Avatar from '../../src/Avatar'
-import styles from './styles.css'
 
 storiesOf('Avatar', module)
-  .add('All types', () => (
-    <div className={styles.box}>
-      <Avatar />
-      <Avatar photo="https://i.imgur.com/V9mgrCp.jpg" />
-      <Avatar
-        photo="https://i.imgur.com/V9mgrCp.jpg"
-        size={50}
-      />
+  .add('Default', () => (
+    <div>
+      <Section title="Default icon and size">
+        <Avatar />
+      </Section>
+
+      <Section title="With custom image and default size">
+        <Avatar photo="https://i.imgur.com/V9mgrCp.jpg" />
+      </Section>
+
+      <Section title="With custom image and size">
+        <Avatar
+          photo="https://i.imgur.com/V9mgrCp.jpg"
+          size={50}
+        />
+      </Section>
     </div>
   ))
 
