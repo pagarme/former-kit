aed993cc4b800076b868f56db1a84a572ad768ff
Deivis Wingert <deiviswingert@hotmail.com>
2018-05-03 14:57:48 -0300

    modal: refactor title

--- src/Modal/ModalTitle.js
+++ src/Modal/ModalTitle.js
@@ -1,16 +1,52 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { isNil } from 'ramda'
+import Button from '../Button'
 import ThemeConsumer from '../ThemeConsumer'
 
 const consumeTheme = ThemeConsumer('UIModal')
 
+const validateCloseProp = (closeIcon, onClose, propName) => {
+  const isCloseProp = propName === 'onClose' || propName === 'closeIcon'
+  if (isCloseProp
+    && !isNil(closeIcon)
+    && isNil(onClose)
+  ) {
+    throw new Error(
+      `The prop onClose must be a function 
+        when the closeIcon prop is received`
+    )
+  }
+}
+
+const validateOnCloseFunction = ({ closeIcon, onClose }, propName) =>
+  validateCloseProp(closeIcon, onClose, propName)
+
+const validateCloseIcon = ({ closeIcon, onClose }, propName) =>
+  validateCloseProp(closeIcon, onClose, propName)
+
 /**
  * Modal component title. It renders its children at the top of the Modal.
  */
-const ModalTitle = ({ theme, title, icon }) => (
+const ModalTitle = ({
+  theme,
+  title,
+  icon,
+  closeIcon,
+  onClose,
+  titleAlign,
+}) => (
   <div className={theme.title}>
-    <div className={theme.icon}>{icon}</div>
-    <h2>{title}</h2>
+    {icon && <div className={theme.icon}>{icon}</div>}
+    <h2 className={theme[`${titleAlign}Align`]}>{title}</h2>
+    {closeIcon &&
+      <Button
+        fill="clean"
+        icon={closeIcon}
+        onClick={onClose}
+        relevance="low"
+      />
+    }
   </div>
 )
 
@@ -19,22 +55,37 @@ ModalTitle.propTypes = {
    * @see [ThemeProvider](#themeprovider) - Theme received from `consumeTheme` wrapper.
    */
   theme: PropTypes.shape({
-    title: PropTypes.string,
     icon: PropTypes.string,
+    title: PropTypes.string,
   }),
+  /**
+   * Close button icon.
+   */
+  closeIcon: validateCloseIcon,
+  /**
+   * The custom icon which stays at the left side of the title.
+   */
+  icon: PropTypes.element,
+  /**
+   * Close action callback.
+   */
+  onClose: validateOnCloseFunction,
   /**
    * Text which will be shown at the top of the Modal.
    */
   title: PropTypes.string.isRequired,
   /**
-   * The custom icon which stays at the left side of the title.
+   * Text title alignment
    */
-  icon: PropTypes.element,
+  titleAlign: PropTypes.oneOf(['center', 'end', 'start']),
 }
 
 ModalTitle.defaultProps = {
+  closeIcon: null,
   icon: null,
+  onClose: null,
   theme: {},
+  titleAlign: 'center',
 }
 
 export default consumeTheme(ModalTitle)
