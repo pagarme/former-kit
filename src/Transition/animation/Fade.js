9eb28cb96c2eb8e41a78be805f96ce09aaab4b2d
lucasjs <lucas.tcjb@gmail.com>
2018-10-16 21:43:10 -0300

    transition: add animations
    Co-authored-by: Deivis <deiviswingert@hotmail.com>

---
+++ src/Transition/animation/Fade.js
@@ -0,0 +1,21 @@
+const Fade = {
+  springOptions: {
+    damping: 30,
+    precision: 0.01,
+    stiffness: 300,
+  },
+  atEnter: {
+    opacity: 0.3,
+  },
+  atActive: {
+    opacity: 1,
+  },
+  atLeave: {
+    opacity: 0,
+  },
+  mapStyles: ({ opacity }) => ({
+    opacity,
+  }),
+}
+
+export default Fade
--- src/Transition/index.js
+++ src/Transition/index.js
@@ -1,1 +1,4 @@
+export { default as Fade } from './animation/Fade'
+export { default as Grow } from './animation/Grow'
 export { default as Transition } from './Transition'
+export { default as Zoom } from './animation/Zoom'
