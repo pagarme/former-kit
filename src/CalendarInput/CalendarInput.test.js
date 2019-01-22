df5a452aa83850c0ee1bd6e1e14179fb332244b9
Deivis <deiviswingert@hotmail.com>
2019-01-22 20:35:19 -0200

    calendar-input: refactor click outside

--- src/CalendarInput/CalendarInput.test.js
+++ src/CalendarInput/CalendarInput.test.js
@@ -24,41 +24,8 @@ const getCalendarInput = (onChange = () => undefined) => (
 )
 
 describe('DateSelector', () => {
-  afterEach(cleanup)
-
   it('should mount component', () => {
     shallow(getCalendarInput())
-  })
-
-  it('should render the Calendar when focused', async () => {
-    const { container } = render(getCalendarInput())
-    let calendar = container.querySelector('.ReactDates-overrides .CalendarMonth')
-    const input = container.querySelector('input')
-
-    expect(calendar).toBeNull()
-
-    fireEvent.focus(input)
-
-    await waitExpect(() => {
-      calendar = container.querySelector('.ReactDates-overrides .CalendarMonth')
-
-      expect(calendar).not.toBeNull()
-    })
-  })
-
-  it('should call onChange when a date is changed', async () => {
-    const onChange = jest.fn()
-    const { container } = render(getCalendarInput(onChange))
-    const input = container.querySelector('input')
-
-    fireEvent.focus(input)
-
-    const calendarButton = await waitForElement(() =>
-      container.querySelector('.CalendarMonth .CalendarDay--valid > .CalendarDay__button'))
-
-    fireEvent.click(calendarButton)
-
-    expect(onChange).toHaveBeenCalledTimes(1)
   })
 
   it('should return start and end properties', () => {
@@ -97,9 +64,9 @@ describe('DateSelector', () => {
     expect(dates.end).toBeInstanceOf(moment)
 
     expect(dates.start.toLocaleString())
-      .toBe(moment('06/27/2018').startOf('day').toLocaleString())
+      .toBe(moment('06/27/2018', 'L').startOf('day').toLocaleString())
     expect(dates.end.toLocaleString())
-      .toBe(moment('06/28/2018').endOf('day').toLocaleString())
+      .toBe(moment('06/28/2018', 'L').endOf('day').toLocaleString())
   })
 
   it('should show only one input when the dateSelection is "single"', () => {
@@ -123,28 +90,63 @@ describe('DateSelector', () => {
     expect(inputs).toBe(2)
   })
 
-  it('should show two calendars when the months props is 2', async () => {
-    const { container } = render(
-      <CalendarInput
-        dateSelection="period"
-        months={2}
-        onChange={() => undefined}
-        value={defaultDates}
-      />)
-    let calendars = container.querySelectorAll('.CalendarMonthGrid > .CalendarMonth')
-    let weekHeaders = container.querySelectorAll('.DayPicker__week-headers > .DayPicker__week-header')
-    const input = container.querySelector('input')
+  describe('events', () => {
+    afterEach(cleanup)
 
-    expect(calendars).toHaveLength(0)
+    it('should call onChange when a date is changed', async () => {
+      const onChange = jest.fn()
+      const { container } = render(getCalendarInput(onChange))
+      const input = container.querySelector('input')
 
-    fireEvent.focus(input)
+      fireEvent.focus(input)
 
-    await waitExpect(() => {
-      calendars = container.querySelectorAll('.CalendarMonthGrid > .CalendarMonth')
-      weekHeaders = container.querySelectorAll('.DayPicker__week-headers > .DayPicker__week-header')
+      const calendarButton = await waitForElement(() =>
+        container.querySelector('.CalendarMonth .CalendarDay__default'))
 
-      expect(weekHeaders).toHaveLength(2)
-      expect(calendars).toHaveLength(4)
+      fireEvent.click(calendarButton)
+
+      expect(onChange).toHaveBeenCalledTimes(1)
+    })
+
+    it('should render the Calendar when focused', async () => {
+      const { container } = render(getCalendarInput())
+      let calendar = container.querySelector('.ReactDates-overrides .CalendarMonth')
+      const input = container.querySelector('input')
+
+      expect(calendar).toBeNull()
+
+      fireEvent.focus(input)
+
+      calendar = await waitForElement(() =>
+        container.querySelector('.ReactDates-overrides .CalendarMonth')
+      )
+
+      expect(calendar).not.toBeNull()
+    })
+
+    it('should show two calendars when the months props is 2', async () => {
+      const { container } = render(
+        <CalendarInput
+          dateSelection="period"
+          months={2}
+          onChange={() => undefined}
+          value={defaultDates}
+        />)
+      let calendars = container.querySelectorAll('.CalendarMonthGrid > .CalendarMonthGrid_month__horizontal')
+      let weekHeaders = container.querySelectorAll('.DayPicker_weekHeaders > .DayPicker_weekHeader')
+      const input = container.querySelector('input')
+
+      expect(calendars).toHaveLength(0)
+
+      fireEvent.focus(input)
+
+      await waitExpect(() => {
+        calendars = container.querySelectorAll('.CalendarMonthGrid > .CalendarMonthGrid_month__horizontal')
+        weekHeaders = container.querySelectorAll('.DayPicker_weekHeaders > .DayPicker_weekHeader')
+
+        expect(weekHeaders).toHaveLength(2)
+        expect(calendars).toHaveLength(4)
+      })
     })
   })
 })
