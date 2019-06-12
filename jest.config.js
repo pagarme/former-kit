56e0063fbab2083b2aafcd66cf364ec23be8761c
gmbarroso <barroso.guilherme@gmail.com>
2019-06-12 17:26:03 -0300

    modal: update snapshot

--- jest.config.js
+++ jest.config.js
@@ -15,7 +15,7 @@ module.exports = {
   transform: {
     "^.+\\.(js|jsx)$": "<rootDir>/node_modules/babel-jest",
     "^.+\\.css$": "<rootDir>/config/jest/cssTransform.js",
-    "^(?!.*\\.(js|jsx|css|json)$)": "<rootDir>/config/jest/fileTransform.js"
+    "^(?!.*\\.(js|jsx|css|json)$)": "<rootDir>/config/jest/fileTransform.js",
   },
   transformIgnorePatterns: [
     "[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$"
