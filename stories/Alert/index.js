2f1eea8615d8537e55334229ba3a4f337ca90638
Eduardo G. S. Pereira <edu_vcd@hotmail.com>
2020-08-07 21:46:57 -0300

    alert: refactor alert colors

--- stories/Alert/index.js
+++ stories/Alert/index.js
@@ -2,10 +2,10 @@ import React from 'react'
 import { action } from '@storybook/addon-actions'
 import { storiesOf } from '@storybook/react'
 
-import IconCheck from 'emblematic-icons/svg/Check32.svg'
-import IconClear from 'emblematic-icons/svg/ClearClose32.svg'
-import IconInfo from 'emblematic-icons/svg/Info32.svg'
-import IconWarning from 'emblematic-icons/svg/Warning32.svg'
+import IconCheck from 'emblematic-icons/svg/CheckCircle32.svg'
+import IconClear from 'emblematic-icons/svg/CloseCircle32.svg'
+import IconInfo from 'emblematic-icons/svg/InfoCircle32.svg'
+import IconWarning from 'emblematic-icons/svg/ExclamationCircle32.svg'
 
 import Alert from '../../src/Alert'
 import AlertReadme from '../../src/Alert/index.md'
@@ -59,58 +59,6 @@ storiesOf('Alerts', module)
       <Section title="Event">
         <Alert
           action="dismiss"
-          icon={<IconWarning height={16} width={16} />}
-          onDismiss={action('dismiss')}
-          type="warning"
-        >
-          <p>Warning. Something is going on!</p>
-        </Alert>
-      </Section>
-
-      <Section title="Warning" base="dark">
-        <Alert
-          base="dark"
-          icon={<IconWarning height={16} width={16} />}
-          type="warning"
-        >
-          <p>Warning. Something is going on!</p>
-        </Alert>
-      </Section>
-
-      <Section title="Info" base="dark">
-        <Alert
-          base="dark"
-          icon={<IconInfo height={16} width={16} />}
-          type="info"
-        >
-          <p>Info. You can do it better!</p>
-        </Alert>
-      </Section>
-
-      <Section title="Error" base="dark">
-        <Alert
-          base="dark"
-          icon={<IconClear height={16} width={16} />}
-          type="error"
-        >
-          <p>Error. Something went wrong!</p>
-        </Alert>
-      </Section>
-
-      <Section title="Success" base="dark">
-        <Alert
-          base="dark"
-          icon={<IconCheck height={16} width={16} />}
-          type="success"
-        >
-          <p>Success. Awesome, it worked!</p>
-        </Alert>
-      </Section>
-
-      <Section title="Event" base="dark">
-        <Alert
-          action="dismiss"
-          base="dark"
           icon={<IconWarning height={16} width={16} />}
           onDismiss={action('dismiss')}
           type="warning"
